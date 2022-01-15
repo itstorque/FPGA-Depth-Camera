c7a89139c19eca047db850e24ee22daa212401a1
tareqdandachi <tareqdandachi@gmail.com>
2022-01-15 10:49:41 -0500

    Multiple camera feeds work

--- constraints.xdc
+++ constraints.xdc
@@ -122,14 +122,14 @@ set_property -dict { PACKAGE_PIN H14   IOSTANDARD LVCMOS33 } [get_ports { jbclk 
 
 ##Pmod Header JC
 
-#set_property -dict { PACKAGE_PIN K1    IOSTANDARD LVCMOS33 } [get_ports { jc[0] }]; #IO_L23N_T3_35 Sch=jc[1]
-#set_property -dict { PACKAGE_PIN F6    IOSTANDARD LVCMOS33 } [get_ports { jc[1] }]; #IO_L19N_T3_VREF_35 Sch=jc[2]
-#set_property -dict { PACKAGE_PIN J2    IOSTANDARD LVCMOS33 } [get_ports { jc[2] }]; #IO_L22N_T3_35 Sch=jc[3]
-#set_property -dict { PACKAGE_PIN G6    IOSTANDARD LVCMOS33 } [get_ports { jc[3] }]; #IO_L19P_T3_35 Sch=jc[4]
-#set_property -dict { PACKAGE_PIN E7    IOSTANDARD LVCMOS33 } [get_ports { jc[4] }]; #IO_L6P_T0_35 Sch=jc[7]
-#set_property -dict { PACKAGE_PIN J3    IOSTANDARD LVCMOS33 } [get_ports { jc[5] }]; #IO_L22P_T3_35 Sch=jc[8]
-#set_property -dict { PACKAGE_PIN J4    IOSTANDARD LVCMOS33 } [get_ports { jc[6] }]; #IO_L21P_T3_DQS_35 Sch=jc[9]
-#set_property -dict { PACKAGE_PIN E6    IOSTANDARD LVCMOS33 } [get_ports { jc[7] }]; #IO_L5P_T0_AD13P_35 Sch=jc[10]
+set_property -dict { PACKAGE_PIN K1    IOSTANDARD LVCMOS33 } [get_ports { jc[0] }]; #IO_L23N_T3_35 Sch=jc[1]
+set_property -dict { PACKAGE_PIN F6    IOSTANDARD LVCMOS33 } [get_ports { jc[1] }]; #IO_L19N_T3_VREF_35 Sch=jc[2]
+set_property -dict { PACKAGE_PIN J2    IOSTANDARD LVCMOS33 } [get_ports { jc[2] }]; #IO_L22N_T3_35 Sch=jc[3]
+set_property -dict { PACKAGE_PIN G6    IOSTANDARD LVCMOS33 } [get_ports { jc[3] }]; #IO_L19P_T3_35 Sch=jc[4]
+set_property -dict { PACKAGE_PIN E7    IOSTANDARD LVCMOS33 } [get_ports { jc[4] }]; #IO_L6P_T0_35 Sch=jc[7]
+set_property -dict { PACKAGE_PIN J3    IOSTANDARD LVCMOS33 } [get_ports { jc[5] }]; #IO_L22P_T3_35 Sch=jc[8]
+set_property -dict { PACKAGE_PIN J4    IOSTANDARD LVCMOS33 } [get_ports { jc[6] }]; #IO_L21P_T3_DQS_35 Sch=jc[9]
+set_property -dict { PACKAGE_PIN E6    IOSTANDARD LVCMOS33 } [get_ports { jc[7] }]; #IO_L5P_T0_AD13P_35 Sch=jc[10]
 
 
 ##Pmod Header JD
--- src/main/kotlin/FpgaTop.kt
+++ src/main/kotlin/FpgaTop.kt
@@ -8,15 +8,23 @@ import imported.VgaClkGen
 
 @SynthTop
 class FpgaTop(
+
     @In var clk_100mhz: Boolean,
+
+    @In var ja: Ubit<`8`>,
+    @In var jb: Ubit<`3`>,
+    @Out var jbclk: Boolean,
+
     @In var jc: Ubit<`8`>,
     @In var jd: Ubit<`3`>,
     @Out var jdclk: Boolean,
+
     @Out var vga_r: Ubit<`4`>,
     @Out var vga_b: Ubit<`4`>,
     @Out var vga_g: Ubit<`4`>,
     @Out var vga_hs: Boolean,
     @Out var vga_vs: Boolean
+
 ) : Module() {
 
     var clk_65mhz: Boolean = nc()
@@ -54,33 +62,74 @@ class FpgaTop(
     var pixel_buff: Ubit<`8`> = nc()
     var pixel_in: Ubit<`8`> = nc()
 
+    var pclk_buff_2: Boolean = nc()
+    var pclk_in_2: Boolean = nc()
+    var vsync_buff_2: Boolean = nc()
+    var vsync_in_2: Boolean = nc()
+    var href_buff_2: Boolean = nc()
+    var href_in_2: Boolean = nc()
+    var pixel_buff_2: Ubit<`8`> = nc()
+    var pixel_in_2: Ubit<`8`> = nc()
+
     var xclk_count: Ubit<`2`> = nc()
 
     @Seq
     fun seqBufferInput() {
         on(posedge(clk_65mhz)) {
-            pclk_buff = jd[0]
-            vsync_buff = jd[1]
-            href_buff = jd[2]
-            pixel_buff = jc
+            pclk_buff = jb[0]
+            vsync_buff = jb[1]
+            href_buff = jb[2]
+            pixel_buff = ja
 
             pclk_in = pclk_buff
             vsync_in = vsync_buff
             href_in = href_buff
             pixel_in = pixel_buff
 
+            pclk_buff_2 = jd[0]
+            vsync_buff_2 = jd[1]
+            href_buff_2 = jd[2]
+            pixel_buff_2 = jc
+
+            pclk_in_2 = pclk_buff_2
+            vsync_in_2 = vsync_buff_2
+            href_in_2 = href_buff_2
+            pixel_in_2 = pixel_buff_2
+
             xclk_count += u(1)
         }
     }
 
+    @Seq
+    fun seqPixelAddr2() {
+        on(posedge(pclk_in_2)) {
+            pixel_addr_in_2 = if (frame_done_out_2) u0() else pixel_addr_in_2 + u(1)
+            pixel_addr_in_2 = when {
+                frame_done_out_2 -> u0()
+                valid_pixel_2 -> pixel_addr_in_2 + u(1)
+                else -> pixel_addr_in_2
+            }
+        }
+    }
+
+    var upscale: Int = 0
+
+    @Com
+    var pixel_addr_out: Ubit<`17`> = (hcount shr upscale) + ((vcount shr upscale) mul u(320)).tru<`17`>()
+
     @Com
     fun comJbclk() {
+        jbclk = xclk_count > u(0b01)
         jdclk = xclk_count > u(0b01)
     }
 
     var output_pixels: Ubit<`16`> = nc()
     var valid_pixel: Boolean = nc()
     var frame_done_out: Boolean = nc()
+
+    var output_pixels_2: Ubit<`16`> = nc()
+    var valid_pixel_2: Boolean = nc()
+    var frame_done_out_2: Boolean = nc()
 
     @Make
     val camera_read = CameraRead(
@@ -93,11 +142,35 @@ class FpgaTop(
         frame_done_out = frame_done_out
     )
 
+    @Make
+    val camera_read_2 = CameraRead(
+        p_clk_in = pclk_in_2,
+        vsync_in = vsync_in_2,
+        href_in = href_in_2,
+        p_data_in = pixel_in_2,
+        pixel_data_out = output_pixels_2,
+        pixel_valid_out = valid_pixel_2,
+        frame_done_out = frame_done_out_2
+    )
+
     var pixel_addr_in: Ubit<`17`> = nc()
+    var pixel_addr_in_2: Ubit<`17`> = nc()
+
+    var COM_1 : Ubit<`17`> = nc()
+    var COM_2 : Ubit<`17`> = nc()
 
     @Seq
     fun seqPixelAddr() {
         on(posedge(pclk_in)) {
+
+            if (frame_done_out) {
+                COM_1 = u0();
+            } else {
+                // get upper three bits of val, and if there sum is greater than const then is part of OBJ
+                var pixel_mag : Ubit<`5`> = frame_buff_out.sli<`3`>(1) add frame_buff_out.sli<`3`>(5) add frame_buff_out.sli<`3`>(9)
+                frame_buff_out = if (pixel_mag > u("5'b01100")) u("12'b1111_0000_0000") else frame_buff_out
+            }
+
             pixel_addr_in = if (frame_done_out) u0() else pixel_addr_in + u(1)
             pixel_addr_in = when {
                 frame_done_out -> u0()
@@ -107,8 +180,25 @@ class FpgaTop(
         }
     }
 
+    @Seq
+    fun seqPixelAddr2() {
+        on(posedge(pclk_in_2)) {
+            pixel_addr_in_2 = if (frame_done_out_2) u0() else pixel_addr_in_2 + u(1)
+            pixel_addr_in_2 = when {
+                frame_done_out_2 -> u0()
+                valid_pixel_2 -> pixel_addr_in_2 + u(1)
+                else -> pixel_addr_in_2
+            }
+        }
+    }
+
+    var upscale: Int = 0
+
     @Com
-    var pixel_addr_out: Ubit<`17`> = (hcount shr 1) + ((vcount shr 1) mul u(320)).tru<`17`>()
+    var pixel_addr_out: Ubit<`17`> = (hcount shr upscale) + ((vcount shr upscale) mul u(320)).tru<`17`>()
+
+    @Com
+    var pixel_addr_out_2: Ubit<`17`> = pixel_addr_out // TODO: same as _1
 
 //    var count: Ubit<`28`> = nc()
 //
@@ -121,24 +211,42 @@ class FpgaTop(
 //    }
 
     @Com
-    var processed_pixels: Ubit<`12`> = //if (count >= cat(u("1'b1"), u("27'b00_0000_0000"))) u("12'b1111_0000_0000") else u("12'b0000_1111_0000")
-        cat(
-            output_pixels.slice<`4`>(12),
-            output_pixels.slice<`4`>(7),
-            output_pixels.slice<`4`>(1)
+    var processed_pixels: Ubit<`12`> = cat(
+            output_pixels.sli<`4`>(12),
+            output_pixels.sli<`4`>(7),
+            output_pixels.sli<`4`>(1)
         )
 
+    @Com
+    var processed_pixels_2: Ubit<`12`> = cat(
+        output_pixels_2.sli<`4`>(12),
+        output_pixels_2.sli<`4`>(7),
+        output_pixels_2.sli<`4`>(1)
+    )
+
     var frame_buff_out: Ubit<`12`> = nc()
+    var frame_buff_out_2: Ubit<`12`> = nc()
 
     @Make
     val blk_mem = CameraBlkMem(
         addra = pixel_addr_in,
         clka = pclk_in,
         dina = processed_pixels,
-        wea = u(valid_pixel),
+        wea = valid_pixel.toUbit(),
         addrb = pixel_addr_out,
         clkb = clk_65mhz,
         doutb = frame_buff_out
+    )
+
+    @Make
+    val blk_mem_2 = CameraBlkMem(
+        addra = pixel_addr_in_2,
+        clka = pclk_in_2,
+        dina = processed_pixels_2,
+        wea = valid_pixel_2.toUbit(),
+        addrb = pixel_addr_out_2,
+        clkb = clk_65mhz,
+        doutb = frame_buff_out_2
     )
 
     var rgb_out: Ubit<`12`> = nc()
@@ -152,18 +260,36 @@ class FpgaTop(
             hsync_out = hsync
             vsync_out = vsync
             blank_out = blank
-            rgb_out = if (hcount < u(320) && vcount < u(240)) frame_buff_out else u0()
-//            rgb_out = if (hcount < u(10) && vcount < u(240)) u("12'b1111_0000_0000") else u0()
-//            rgb_out = if (hcount < u(20) && vcount < u(240)) u("12'b0000_1111_0000") else u0()
-//            rgb_out = if (hcount < u(50) && vcount < u(240)) u("12'b1111_0000_0000") else u0()
+
+            var rgb_out_val : Ubit<`12`>  = if (hcount < u(320) && vcount < u(240)) frame_buff_out else u0()
+            rgb_out_val = if (hcount > u(320) && hcount < u(640) && vcount < u(240)) frame_buff_out_2 else rgb_out_val
+
+            rgb_out = rgb_out_val
         }
     }
 
+    @Seq
+    fun seqPixelAddr2() {
+        on(posedge(pclk_in_2)) {
+            pixel_addr_in_2 = if (frame_done_out_2) u0() else pixel_addr_in_2 + u(1)
+            pixel_addr_in_2 = when {
+                frame_done_out_2 -> u0()
+                valid_pixel_2 -> pixel_addr_in_2 + u(1)
+                else -> pixel_addr_in_2
+            }
+        }
+    }
+
+    var upscale: Int = 0
+
+    @Com
+    var pixel_addr_out: Ubit<`17`> = (hcount shr upscale) + ((vcount shr upscale) mul u(320)).tru<`17`>()
+
     @Com
     fun comOutput() {
-        vga_r = if (!blank_out) rgb_out.slice(8) else u0()
-        vga_g = if (!blank_out) rgb_out.slice(4) else u0()
-        vga_b = if (!blank_out) rgb_out.slice(0) else u0()
+        vga_r = if (!blank_out) rgb_out.sli(8) else u0()
+        vga_g = if (!blank_out) rgb_out.sli(4) else u0()
+        vga_b = if (!blank_out) rgb_out.sli(0) else u0()
         vga_hs = !hsync_out
         vga_vs = !vsync_out
     }
